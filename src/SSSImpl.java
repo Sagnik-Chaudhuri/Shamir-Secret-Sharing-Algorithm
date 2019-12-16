e9535d01b55c20968e67c9b270b6b11b44bec455
Sagnik-Chaudhuri <csagnik.chaudhuri@gmail.com>
2019-12-17 00:46:56 +0530

    Update SSSImpl.java

--- src/SSSImpl.java
+++ src/SSSImpl.java
@@ -1,12 +1,13 @@
 import java.security.SecureRandom;
 import java.util.Map;
 import java.util.Scanner;
+
 public class SSSImpl {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n, k;
 		System.out.println("Enter number of shares and k");
 		n = sc.nextInt();
@@ -14,26 +15,19 @@ public class SSSImpl {
 		final Scheme scheme = new Scheme(new SecureRandom(), n, k);
 		System.out.println("Enter number of secrets");
 		int m = sc.nextInt();
-		for(int i = 0; i < m; i++)
-		{
+		for (int i = 0; i < m; i++) {
 			System.out.println("Enter Secret");
 
 			final byte[] secret = new byte[32];
-			for(int i1 = 0; i1 < 32; i1++)
-			{
-				secret[i1] =  sc.nextByte();
+			for (int i1 = 0; i1 < 32; i1++) {
+				secret[i1] = sc.nextByte();
 			}
 			final Map<Integer, byte[]> parts = scheme.split(secret);
 			final byte[] recovered = scheme.join(parts);
-			
-			System.out.println("Recovered Secret: "+ recovered.length);
-			for(int i1 = 0; i1 < 32; i1++)
+
+			System.out.println("Recovered Secret: " + recovered.length);
+			for (int i1 = 0; i1 < 32; i1++)
 				System.out.print(recovered[i1]);
-//			final byte[] secret = "Hello".getBytes(StandardCharsets.UTF_8);
-//		    final Map<Integer, byte[]> parts = scheme.split(secret);
-//		    final byte[] recovered = scheme.join(parts);
-//		    System.out.println("asdad"+new String(recovered, StandardCharsets.UTF_8));
-			
 		}
 		sc.close();
 
